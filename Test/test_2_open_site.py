b57ed0ce475ecb764512f1e11f63f52949b4a311
Alina <sharunova-alina2014@yandex.ru>
2025-03-15 12:53:34 +0300

    First Commit

--- Test/test_2_open_site.py
+++ Test/test_2_open_site.py
@@ -2,7 +2,7 @@ from selenium.webdriver.commom.by import By
 from selenium import webdriver
 
 driver = webdriver.Chrome()
-driver.get('https://demoqa.com/')
+driver.get("https://demoqa.com/")
 
 # поиск элемeнта
 icon = driver.find_element(By.CSS_SELECTOR,'header > a > img') #переменная iconб к которой обращаемся через драйвер с помощью метода find_element(первым эл-том передаём тип поиска,вторым тип локатора, который будем искать)
