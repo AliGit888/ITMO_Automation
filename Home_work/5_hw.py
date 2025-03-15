29eb74d31718c0a48f0483f37b27be1bcf329374
Alina <sharunova-alina2014@yandex.ru>
2025-03-15 16:40:54 +0300

    First Commit

---
+++ Home_work/5_hw.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
+
+username_field = driver.find_element(By.ID, "user-name")
+password_field = driver.find_element(By.ID, "password")
+login_button = driver.find_element(By.ID, "login-button")
+
+if username_field and password_field and login_button is None:
+    print("Элемент(ы) не найдены")
+else:
+    print("Элементы найдены")
+
