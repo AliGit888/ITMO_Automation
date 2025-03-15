6dbce25ddb9fea52797a31d09af4fb64276a890b
Alina <sharunova-alina2014@yandex.ru>
2025-03-15 11:51:16 +0300

    First Commit

--- python_trening/test_8_if_elif_else.py
+++ python_trening/test_8_if_elif_else.py
@@ -1,30 +1,30 @@
-# num = 100
-# if num >=0:
-#     print ('число больше либо равно 0')
-# else:
-#     print('число отрицательное')
-#
-# def task_yes_no(str_1,str_2):
-#     if str_1 in str_2:
-#         print ('да')
-#     else:
-#         print('нет')
-# task_yes_no(str_1='test', str_2='test1')
-#
-# num_float=4.5
-# if num_float>0:
-#     print ('Положительное число')
-# elif num_float==0:
-#     print ('Ноль')
-# else:
-#     print('Чило отрицательно')
+num = 100
+if num >=0:
+    print ('число больше либо равно 0')
+else:
+    print('число отрицательное')
 
-# permit_print = True
-# num=-3
-# if num>0 and permit_print:
-#     print('num - положительное число')
-# elif not permit_print or num <0:
-#     print('Печать запрещена')
+def task_yes_no(str_1,str_2):
+    if str_1 in str_2:
+        print ('да')
+    else:
+        print('нет')
+task_yes_no(str_1='test', str_2='test1')
+
+num_float=4.5
+if num_float>0:
+    print ('Положительное число')
+elif num_float==0:
+    print ('Ноль')
+else:
+    print('Чило отрицательно')
+
+permit_print = True
+num=-3
+if num>0 and permit_print:
+    print('num - положительное число')
+elif not permit_print or num <0:
+    print('Печать запрещена')
 
 def kurs(year_of_study):
     if year_of_study == 1 or year_of_study ==2 or year_of_study == 3 or year_of_study == 4:
