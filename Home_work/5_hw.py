3974e70cea4b5676156a59417f0a1b9b965f4ad8
Alina <sharunova-alina2014@yandex.ru>
2025-03-18 21:30:51 +0300

    First Commit

--- Home_work/5_hw.py
+++ Home_work/5_hw.py
@@ -1,14 +1,16 @@
-def five_hw():
-    from selenium import webdriver
-    from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+def open_site():
     driver = webdriver.Chrome()
     driver.get("https://www.saucedemo.com/")
 
+def find_element(driver):
     username_field = driver.find_element(By.ID, "user-name")
     password_field = driver.find_element(By.ID, "password")
     login_button = driver.find_element(By.ID, "login-button")
 
+def check_element(username_field,password_field,login_button):
     if username_field and password_field and login_button is None:
         print("Элемент(ы) не найдены")
     else:
